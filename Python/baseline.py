b8168c7b88961a5772cda3ccd7574339d33b65b2
SandraShu <zhangshugar@163.com>
2018-03-28 16:23:04 +0800

    update

--- Python/baseline.py
+++ Python/baseline.py
@@ -59,16 +59,22 @@ def base_process(data):
     data['len_predict_category_property'] = data['predict_category_property'].map(lambda x: len(str(x).split(';')))
     print("  =========> Part 1!") 
     
-    singleIntFeatureList = singleIntFeature + ['instance_id']
+    # one-hot
+    #singleIntFeatureList = singleIntFeature + ['instance_id']
+    # id 类型
+    singleIntFeature = ['item_id','shop_id','item_city_id','item_brand_id']
+    singleIntFeatureList = ['item_id','shop_id','item_city_id','item_brand_id','instance_id']
     category = data.loc[:,singleIntFeatureList]
     category.loc[:,singleIntFeature] = category.loc[:,singleIntFeature].astype('str')
     dfCategory = pd.get_dummies(category)
     data = pd.merge(data,dfCategory,on='instance_id')
     print("  =========> Part 2!") 
     
+    '''
+    # 分箱 + one-hot
     for x in singleDoubleShop:            
         ser = data[x]            
-        cats = pd.cut(ser[ser != -1], 10, labels=[1,2,3,4,5,6,7,8,9,10])
+        cats = pd.qcut(ser[ser != -1], 10, labels=[1,2,3,4])
         ser = pd.concat([cats, ser[ser == -1]]).astype('int')
         data[x+'_dispersed'] = ser
     
@@ -78,41 +84,16 @@ def base_process(data):
     dfCategory = pd.get_dummies(category)
     data = pd.merge(data,dfCategory,on='instance_id')
     print("  =========> Part 3!") 
+    '''
+    # todo: join
     
     return data 
 
-if __name__ == "__main__":
+def main(onlineFlag):
     
-    online = False# 这里用来标记是 线下验证 还是 在线提交
     path = '../Data/'
-
-    data = pd.read_csv(path + 'round1_ijcai_18_train_20180301.txt', sep=' ')
-    #data = pd.read_csv(path + 'sample.txt', sep=' ')
-    print('Load data success!')
-     
-    data.drop_duplicates(subset='instance_id')
-    data = convert_time(data)
-    data = base_process(data)
-    print('Pre-process success!')
     
-    item_history_cvr = pd.read_csv(path + 'features/item_id_history.csv')
-    print('Load history data success!') 
-    
-    data = pd.merge(data,item_history_cvr[item_history_cvr['isTrain'] == 1],on='instance_id')
-    print('Merge history data success!')    
-    
-    
-    if online == False:
-        train = data.loc[data.day < 24]  # 18,19,20,21,22,23
-        test = data.loc[data.day == 24]  # 暂时先使用第24天作为验证集
-    elif online == True:
-        train = data.copy()
-        test = pd.read_csv('../data/round1_ijcai_18_test_a_20180301.txt', sep=' ')
-        print(len(test))
-        test = pd.merge(test,item_history_cvr[item_history_cvr['isTrain'] == 0],on='instance_id')
-        test = convert_time(test)
-
-    gbdt_features = ['item_price_level', 'item_sales_level','item_collected_level', 'item_pv_level', 
+    gbdt_features = ['item_price_level', 'item_sales_level','item_collected_level', 'item_pv_level', 'context_page_id',
                      'user_age_level', 'user_star_level', 'user_query_day', 'user_query_day_hour',
                      'hour', 'shop_review_num_level', 'shop_star_level','shop_review_positive_rate', 
                      'shop_score_service', 'shop_score_delivery', 'shop_score_description','cvr','last_day_cvr',
@@ -120,13 +101,72 @@ if __name__ == "__main__":
     
     # init lr feature
     UselessFeature = idList + singleDoubleShopDispersed + singleDoubleShop + singleIntFeature + listItem + unsureList + timeFeature + label + gbdt_features
-    lr_features=[x for x in data.columns if x not in UselessFeature]
     
     target = ['is_trade']
-
-    if online == False:
-        gbdt_lr_train.gbdt_lr_train(train,test,gbdt_features,lr_features,target)
-    else:
-        print('Onlinr]e')
+    
+    if onlineFlag == True:
+        
+        # load train
+        train = pd.read_csv(path + 'round1_ijcai_18_train_20180301.txt', sep=' ')
+        train.drop_duplicates(subset='instance_id')
+        train['isTrain'] = 1
+        
+        # load test
+        test = pd.read_csv('../data/round1_ijcai_18_test_a_20180301.txt', sep=' ')
+        test['isTrain'] = 0
+        print(len(test))
+        
+        # concat and process
+        data = pd.concat([train,test])
+        data = convert_time(data)
+        data = base_process(data)
+        
+        lr_features=[x for x in data.columns if x not in UselessFeature]
+        
+        # load history data
+        item_history_cvr = pd.read_csv(path + 'features/item_id_history.csv')
+        print('Load history data success!') 
+        
+        # merge data with history data
+        trainData = pd.merge(data[data['isTrain'] == 1],item_history_cvr[item_history_cvr['isTrain'] == 1],on='instance_id')
+        testData = pd.merge(data[data['isTrain'] == 0],item_history_cvr[item_history_cvr['isTrain'] == 0],on='instance_id')
+        print('Merge history data success!')  
+        
         #gbdt_lr_train.predict(train[features],train[target],test,features,'with_history_cvr_and_last_day_cvr')
+        
+    else:
+        
+        # load train
+        data = pd.read_csv(path + 'round1_ijcai_18_train_20180301.txt', sep=' ')
+        print('Load data success!')
+        
+        # process data
+        data.drop_duplicates(subset='instance_id')
+        data = convert_time(data)
+        data = base_process(data)
+        print('Pre-process success!')
+        
+        # load history data
+        item_history_cvr = pd.read_csv(path + 'features/item_id_history.csv')
+        print('Load history data success!') 
+        
+        # merge data with history data
+        data = pd.merge(data,item_history_cvr[item_history_cvr['isTrain'] == 1],on='instance_id')
+        print('Merge history data success!')  
+        
+        lr_features=[x for x in data.columns if x not in UselessFeature]
+        
+        # split train and validate data
+        train = data.loc[data.day < 24]  # 18,19,20,21,22,23
+        test = data.loc[data.day == 24]  # 暂时先使用第24天作为验证集
+        
+        # train
+        gbdt_lr_train.gbdt_lr_train(train,test,gbdt_features,lr_features,target)
+            
+
+if __name__ == "__main__":
+    
+    online = False # 这里用来标记是 线下验证 还是 在线提交
+    main(online)
+        
 
